1e52ab4bdc92c7ac270c01f8e83e5269d1d74b38
Abhs15github <abhitam33@gmail.com>
2024-02-28 18:08:45 +0530

    as

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ import { getAuth } from 'firebase-admin/auth';
 import aws from "aws-sdk";
 import jwt from 'jsonwebtoken';
 import Notification from './Schema/Notification.js';
+import Comment from "./Schema/Comment.js"
 
 const server = express();
 const PORT = 3000;
@@ -557,7 +558,75 @@ server.post("/isliked-by-user", verifyJWT, (req, res) => {
 
 })
 
+server.post("/add-comment", verifyJWT, (req, res) => {
+  let user_id = req.user;
+  let { _id, comment, blog_author, replying_to } = req.body;
+//to be resumed
+  if(!comment.length){ 
+    return res.status(403).json({ error: "Kindly enter your comment" });
+  }
 
+  let commentObj = {
+    blog_id: _id, blog_author, comment, commented_by: user_id, 
+  }
+
+  if(replying_to){
+    commentObj.parent = replying_to;
+  }
+
+  new Comment(commentObj).save().then(async commentFile => {
+
+    let { comment, commentedAt, children } = commentFile;
+    Blog.findOneAndUpdate({ _id}, { $push: { "comments": commentFile._id}, $inc : { "activity.total_comments": 1, "activity.total_parent_comments": replying_to ? 0 : 1 }, })
+    .then(blog => { console.log('Commented')});
+
+    let notificationObj = {
+      type: replying_to ? "reply" : "comment",
+      blog: _id,
+      notification_for: blog_author,
+      user: user_id, 
+      comment: commentFile._id
+    }
+
+    if(replying_to){
+      notificationObj.replied_on_comment = replying_to;
+
+      await Comment.findOneAndUpdate({ _id: replying_to }, { $push: { children: commentFile._id } })
+      .then(replyingToCommentDoc => { notificationObj.notification_for = replyingToCommentDoc.commented_by })
+
+      
+    }
+
+    new Notification(notificationObj).save().then(notification => console.log('new notification appeared'));
+
+    return res.status(200).json({
+      comment, commentedAt, _id: commentFile._id, user_id, children
+    })
+
+  })
+
+
+})
+
+server.post("/get-blog-comments", (req, res) => {
+  let { blog_id, skip } = req.body;
+  let maxLimit = 5;
+
+  Comment.find({ blog_id, isReply: false })
+  .populate("commented_by", "personal_info.username personal_info.fullname personal_info.profile_img")
+  .skip(skip)
+  .limit(maxLimit)
+  .sort({
+    'commenntedAt': -1
+  })
+  .then(comment => {
+    return res.status(200).json(comment);
+  })
+  .catch(err => {
+    console.log(err.message);
+    return res.status(500).json({ error: err.message })
+  })
+})
 
 
 server.listen(PORT, () => {
